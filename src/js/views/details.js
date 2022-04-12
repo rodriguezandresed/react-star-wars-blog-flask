3e1ac6476ff03cfd92f5706637c39fe02723649a
Andres Rodriguez <rodriguezandresed@gmail.com>
2022-04-12 18:50:31 -0400

    [add] fixed details

--- src/js/views/details.js
+++ src/js/views/details.js
@@ -1,29 +1,63 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext"
 
 export const Details = () => {
 const params = useParams();
 const {store} = useContext(Context);
+const [detailLocal, setDetailLocal] = useState({});
 
 const {nature, uid} = params;
-console.log(params);
 const getDetails = () => {
     const detail = store[nature].find(item => item.uid === uid);
+    
     if (detail){
-        console.log(detail);
+        setDetailLocal(detail.properties);   
+      
     }
 
 }
-
 useEffect(()=>{
     getDetails()
 }, []);
 
-// const character = store.people.find(c => c.uid === id);
+
+
 return (
 
-<h1>soy details  </h1>
+<div className="container">
+<div className="row">
+<div className="card mb-3">
+  <div className="row g-0">
+    <div className="col-md-4">
+      <img src={`https://via.placeholder.com/400x200`} className="img-fluid rounded-start" alt="..."/>
+    </div>
+    <div className="col-md-8">
+      <div className="card-body">
+        <h5 className="card-title">{detailLocal.name}</h5>
+        <p className="card-text">Gender: {detailLocal.gender}</p>
+        <p className="card-text">Hair color: {detailLocal.hair_color}</p>
+        <p className="card-text">Eyes color: {detailLocal.eye_color}</p> 
+      </div>
+    </div>
+  </div>
+</div> 
+
+</div>
+
+<div className="row line"></div>
+<div class="row row-cols-6">
+    <div class="col">Column</div>
+    <div class="col">Column</div>
+    <div class="col">Column</div>
+    <div class="col">Column</div>
+    <div class="col">Column</div>
+    <div class="col">Column</div>
+  </div>
+
+
+</div>
+
 
 
     
